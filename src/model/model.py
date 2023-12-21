64830d824aaad74e685364305606b757ec3e153b
Võ Đình Đạt <vodinhdat03@gmail.com>
2023-12-21 10:20:27 +0700

    update model, update pyproject

--- src/model/model.py
+++ src/model/model.py
@@ -1,100 +1,103 @@
-from model.evaluate import evaluate
-from sklearn.ensemble import (
-    RandomForestRegressor,
-    VotingRegressor,
-    GradientBoostingRegressor,
-    StackingRegressor
-)
-from sklearn.linear_model import (
-    LinearRegression,
-    ElasticNetCV,
-    RidgeCV,
-    LassoCV
-
-)
-
-import pickle
-import os
+from xgboost import XGBRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import polars as pl
 import numpy as np
+import math
 
 
-class Model:
+class LaptopPredictionModel:
     def __init__(
         self,
-        seed: int,
-        checkpoint: str = None
+        model: str,
+        columns: list,
+        params: dict | None = None
     ):
+        if model == "xgb":
+            self.model = XGBRegressor()
+            self.grid_search = True
+            self.scaler = False
+        elif model == "gdb":
+            self.model = GradientBoostingRegressor()
+            self.grid_search = True
+            self.scaler = False
+        elif model == "rdf":
+            self.model = RandomForestRegressor()
+            self.grid_search = True
+            self.scaler = False
+        elif model == "lnr":
+            self.model = LinearRegression()
+            self.grid_search = False
+            self.scaler = True
 
-        linear = LinearRegression()
-        elastic = ElasticNetCV()
-        ridge = RidgeCV()
-        lasso = LassoCV()
-        
-        random_forest = RandomForestRegressor(
-            n_estimators=100,
-            max_depth=10,
-            criterion="friedman_mse",
-            random_state=seed
-        )
-
-        gradient_boosting = GradientBoostingRegressor(
-            n_estimators=100,
-            max_depth=10,
-            criterion="friedman_mse",
-            random_state=seed
-        )
-
-        voting = VotingRegressor(
-            estimators=[
-                ("lr", LinearRegression()),
-                ("ridge", RidgeCV()),
-                ("lasso", LassoCV()),
-                ("elastic", ElasticNetCV())
-            ],
-            n_jobs=-1
-        )
-
-        stacking = StackingRegressor(
-            estimators=[
-                ("lr", LinearRegression()),
-                ("ridge", RidgeCV()),
-                ("lasso", LassoCV()),
-                ("elastic", ElasticNetCV())
-            ],
-            n_jobs=-1
-        )
-
-        self.model_dict = {
-            "linear": linear,
-            "elastic": elastic,
-            "ridge": ridge,
-            "lasso": lasso,
-            "random_forest": random_forest,
-            "gradient_boosting": gradient_boosting,
-            "voting": voting,
-            "stacking": stacking
-        }
-
-        self.checkpoint = checkpoint
+        self.params = params
+        self.columns = columns
 
     def fit(
         self,
         X_train: np.ndarray,
-        y_train: np.ndarray,
         X_test: np.ndarray,
+        y_train: np.ndarray,
         y_test: np.ndarray
     ):
-        for name, model in self.model_dict.items():
-            model.fit(X_train, y_train)
+        print(f"Training {self.model.__class__.__name__}")
+        if self.grid_search:
+            self.grid = GridSearchCV(
+                estimator=self.model,
+                param_grid=self.params,
+                cv=5,
+                verbose=1,
+                n_jobs=-1
+            )
+            self.grid.fit(X_train, y_train)
+            y_pred = self.grid.predict(X_test)
+        if self.scaler:
+            x_scaler = StandardScaler()
+            y_scaler = StandardScaler()
+            X_scale = x_scaler.fit_transform(X_train)
+            y_scale = y_scaler.fit_transform(y_train.reshape(-1, 1))
+            x_test_scale = x_scaler.transform(X_test)
+            self.model.fit(X_scale, y_scale)
+            y_pred_scale = self.model.predict(x_test_scale)
+            y_pred = y_scaler.inverse_transform(y_pred_scale)
+        else:
+            self.model.fit(X_train, y_train)
+            y_pred = self.model.predict(X_test)
+            # plot feature importance
+        if self.grid_search:
+            self.feature_importances = self.grid.best_estimator_.feature_importances_
+            self._plot_feature_importance(x=range(X_train.shape[1]))
+        else:
+            self._plot_coef(coef=self.model.coef_)
 
-            y_pred = model.predict(X_test)
-            df = evaluate(name, y_test, y_pred)
-            print(df)
+        metrics = {
+            "Model": self.model.__class__.__name__,
+            'RMSE': round(math.sqrt(mean_squared_error(y_test, y_pred)), 4),
+            'MAE': round(mean_absolute_error(y_test, y_pred), 4),
+            'R2 Score': round(r2_score(y_test, y_pred), 4),
+        }
+        metrics = pl.DataFrame(metrics)
+        print(metrics)
 
-        self._save_model(name)
+    def _plot_feature_importance(self, x):
+        indices = np.argsort(self.feature_importances)[::-1]
+        # plot feature importance and transpose to horizontal
+        plt.figure(figsize=(10, 5))
+        plt.title("Feature Importance")
+        plt.bar(x, self.feature_importances[indices], align='center')
+        plt.xticks(x, self.columns[indices], rotation=90)
+        plt.tight_layout()
+        plt.show()
 
-    def _save_model(self):
-        if not os.path.exists(self.checkpoint):
-            os.makedirs(self.checkpoint)
-        with open(os.path.join(self.checkpoint, 'model.pkl'), 'wb') as f:
-            pickle.dump(self.model_dict, f)
+    def _plot_coef(self, coef):
+        coefs = pl.DataFrame(
+            coef, columns=["Coefficients"], index=self.columns
+        )
+        coefs.plot(kind="barh", figsize=(9, 7))
+        plt.axvline(x=0, color=".5")
+        plt.subplots_adjust(left=0.3)
