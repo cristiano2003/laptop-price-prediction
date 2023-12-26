7b856bab15653842c7a3554a13f8c4ce10666ac8
Võ Đình Đạt <vodinhdat03@gmail.com>
2023-12-27 00:36:42 +0700

    update data, eda, model, checkpoint

--- src/model/model.py
+++ src/model/model.py
@@ -7,8 +7,8 @@ from sklearn.ensemble import (
     AdaBoostRegressor,
     BaggingRegressor
 )
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, r2_score
 import matplotlib.pyplot as plt
 import polars as pl
 import pandas as pd
@@ -48,6 +48,10 @@ class LaptopPredictionModel:
             self.model = BaggingRegressor()
             self.grid_search = True
             self.scaler = False
+        elif model == "sgd":
+            self.model = SGDRegressor(max_iter=10000)
+            self.grid_search = False
+            self.scaler = True
 
         self.params = params
         self.columns = columns
@@ -74,15 +78,11 @@ class LaptopPredictionModel:
             self._save_model()
         if self.scaler:
             x_scaler = StandardScaler()
-            y_scaler = StandardScaler()
             X_scale = x_scaler.fit_transform(X_train)
-            y_scale = y_scaler.fit_transform(y_train.reshape(-1, 1))
             x_test_scale = x_scaler.transform(X_test)
-            self.model.fit(X_scale, y_scale)
-            y_pred_scale = self.model.predict(x_test_scale)
-            y_train_scale = self.model.predict(X_scale)
-            y_pred_train = y_scaler.inverse_transform(y_train_scale)
-            y_pred = y_scaler.inverse_transform(y_pred_scale)
+            self.model.fit(X_scale, y_train)
+            y_pred = self.model.predict(x_test_scale)
+            y_pred_train = self.model.predict(X_scale)
         else:
             self.model.fit(X_train, y_train)
             y_pred_train = self.model.predict(X_train)
@@ -91,7 +91,7 @@ class LaptopPredictionModel:
 
         try:
             if self.grid_search:
-                plt.figure(figsize=(18, 6))
+                plt.figure(figsize=(20, 6))
                 self.feature_importances = self.grid.best_estimator_.feature_importances_
                 self._plot_feature_importance(x=range(X_train.shape[1]))
             else:
@@ -105,8 +105,8 @@ class LaptopPredictionModel:
 
         metrics = {
             "Model": self.model.__class__.__name__,
-            'RMSE Train': round(math.sqrt(mean_squared_error(y_train, y_pred_train)), 4),
-            'RMSE Test': round(math.sqrt(mean_squared_error(y_test, y_pred)), 4),
+            'MAPE Train (%)': round(math.sqrt(mean_absolute_percentage_error(y_train, y_pred_train)), 4)*100,
+            'MAPE Test (%)': round(math.sqrt(mean_absolute_percentage_error(y_test, y_pred)), 4)*100,
             'MAE Train': round(mean_absolute_error(y_train, y_pred_train), 4),
             'MAE Test': round(mean_absolute_error(y_test, y_pred), 4),
             'R2 Score Train': round(r2_score(y_train, y_pred_train), 4),
